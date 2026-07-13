bf73c981e5c614c10a8f05aac8816b9a2d572052
shabnamjabeenkhan <shabnam.jabeen.khan123@gmail.com>
2026-07-14 00:13:01 +0100

    env added

--- coderco-challenge/app.py
+++ coderco-challenge/app.py
@@ -2,15 +2,20 @@ from flask import Flask
 import redis
 import os
 
-r = redis.Redis(host="redisDatabase4", port=6379)
+
+redis_host = os.getenv("REDIS_HOST", "redis")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(host=redis_host, port=redis_port)
 app =Flask(__name__)
 @app.route("/")
 def welcome():
-    return "Welcome!"
+    return "Welcome to the CoderCo Challenge!"
 
 @app.route("/count")
 def visit():
-    return "Count Page"
+   visit_count = r.incr("visits")
+   return f"This page has been visited {visit_count} times!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)    
