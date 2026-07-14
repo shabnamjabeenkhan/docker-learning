03fcb7ab93d492107628c9ef066342504c090c86
shabnamjabeenkhan <shabnam.jabeen.khan123@gmail.com>
2026-07-14 01:48:13 +0100

    html css added

--- coderco-challenge/app.py
+++ coderco-challenge/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import redis
 import os
 
@@ -10,12 +10,18 @@ r = redis.Redis(host=redis_host, port=redis_port)
 app =Flask(__name__)
 @app.route("/")
 def welcome():
-    return "Welcome to the CoderCo Challenge!"
+    return render_template(
+        "index.html",
+        title="CoderCo Challenge",
+        message="Welcome to VisitTracker Pro by CoderCo!",
+        paragraph="Click the button below to view the number of times this page has been visited.",
+        button_text="View Visit Count",
+    )
 
 @app.route("/count")
 def visit():
    visit_count = r.incr("visits")
-   return f"This page has been visited {visit_count} times!"
+   return render_template("count.html", visit_count=visit_count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)    
